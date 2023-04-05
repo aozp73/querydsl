5df1af4f1f865b2ceadd81cb278326c0b0fc3e43
aozp73 <aozp73@naver.com>
2023-04-05 12:19:07 +0900

    Initial commit

---
+++ src/test/java/shop/mtcoding/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
